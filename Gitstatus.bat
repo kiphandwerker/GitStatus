e7f5382bd6e678679af1b8801e7a2b6048fde3a5
JKH <jkhndwrk@gmail.com>
2025-06-03 10:10:22 -0500

    Added user options for running the script, opening the output, or exiting

--- Gitstatus.bat
+++ Gitstatus.bat
@@ -1,1 +1,19 @@
-start "" "C:/Users/jhandwer/OneDrive - St. Jude Children's Research Hospital/Programs/Cpp/FindGits/FindGits.exe"
+@echo off
+echo.
+echo What would you like to do?
+echo 1. Run the script
+echo 2. Open git_repo_status
+echo 3. Quit out of terminal
+set /p choice=Enter your choice: 
+
+if "%choice%"=="1" (
+    echo Running the script...
+    start "" "C:/Users/19018/OneDrive/Programs/C/Cpp/Gitstatus/FindGits.exe"
+) else if "%choice%"=="2" (
+    echo Opening file...
+    start "" "C:/Users/19018/OneDrive/Programs/git_repo_status.txt"
+) else if "%choice%"=="3" (
+    exit
+) else (
+    echo Invalid choice. Exiting.
+)
